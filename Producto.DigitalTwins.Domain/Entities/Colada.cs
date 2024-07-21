023000b4b206188b9486900890d3b44bdb126ee3
MARQUEZ Jesu Amet HP <C.JMARQUEZ@ternium.com>
2024-07-21 13:14:32 -0300

    Clean Architecture

---
+++ Producto.DigitalTwins.Domain/Entities/Colada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producto.DigitalTwins.Domain.Entities
+{
+    public class Colada
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public int Numero { get; init; }
+        public DateTime FechaCreacion { get; } = DateTime.UtcNow;
+    }
+}
